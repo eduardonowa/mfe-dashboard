dc4b1b8444cf5bfaf3ef71dfb61268893a294a93
eduardonowa <eduardo_nowa@hotmail.com>
2024-08-27 20:12:39 -0300

    added auth subapp

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import {Switch, Route, Router} from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
@@ -9,15 +9,15 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma', // Ao invés de gerar as classes em produção com o prefixo default de 'jss', vai gerar com prefixo 'ma' evitando o conflito de classes
 })
 
-export default () => {
+export default ({history}) => {
     return <div>
         <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+            <Router history={history}>
             <Switch>
                 <Route exact path="/pricing" component={Pricing}/>
                 <Route path="/" component={Landing}/>
             </Switch>
-            </BrowserRouter>
+            </Router>
         </StylesProvider>
     </div>
 }
